6028a40f700ee7bfc64e68a32641529659c73e56
Seben <zac.fryer@kingsway.school.nz>
2026-08-03 18:18:26 +1200

    Dash ability

--- PlayerMovementScript.cs
+++ PlayerMovementScript.cs
@@ -7,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public Rigidbody2D myRigidbody; 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
+    [SerializeField] private TrailRenderer tr;  
 
     private float horizontal;
     public float jumpStrength;
     public float moveSpeed;
     private bool isFacingRight = true;
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingTime;
+    public float dashingPower = 24f;
+    private float dashingCooldown = 1f;
 
 
     // Start is called before the first update frame
@@ -24,6 +29,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per every frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
@@ -46,6 +56,12 @@ public class PlayerMovement : MonoBehaviour
             myRigidbody.linearVelocity = new Vector2(myRigidbody.linearVelocityX, myRigidbody.linearVelocityY * 0.5f);
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+
         Flip();
 
     }
@@ -53,6 +69,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         myRigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, myRigidbody.linearVelocityY);
     }
 
@@ -70,6 +91,22 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+       private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = myRigidbody.gravityScale;
+        myRigidbody.gravityScale = 0f;
+        myRigidbody.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        myRigidbody.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
 }
