98d91b6dc0a7f1d49a9552b320cddad009947982
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2026-08-13 09:27:16 +1200

    Ranged weapon - projectile movement

---
+++ Bullet.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic; 
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float normalBulletSpeed = 15f;
+    private Rigidbody2D rb; 
+
+    private void start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+
+        SetStraightVelocity(); 
+    }
+
+private void SetStraightVelocity()
+    {
+        rb.linearVelocity = transform.right * normalBulletSpeed;
+    }
+}
+
+ 
