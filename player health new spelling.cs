6d714b917d031fbe1a65d5684f40c11558838357
timmy-tuff-knuckles <judah.thornton@kingsway.school.nz>
2026-08-19 17:43:47 +1200

    a

---
+++ player health new spelling.cs
@@ -0,0 +1,21 @@
+//player health
+using system.collections;
+using system.collections.generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
