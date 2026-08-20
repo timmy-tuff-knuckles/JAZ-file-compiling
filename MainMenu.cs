387637cacc904bcda2b6f461541ea9cbac728cca
Seben <zac.fryer@kingsway.school.nz>
2026-08-20 17:27:51 +1200

    Main Menu - Added controls menu + two working buttons

--- MainMenu.cs
+++ MainMenu.cs
@@ -6,7 +6,19 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {   
         //loads scene 1 - level 1
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void controls()
+    {
+        //Loads scene 10 - which is the control screen
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void back()
+    {
+        //Loads scene 0 - which is the main menu
+        SceneManager.LoadSceneAsync(0);
     }
 
     // Update is called once per frame
